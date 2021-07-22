14459c8329ce9f9a7c53af60e68775d0f6ac14f5
cordea <cordear@outlook.com>
2021-07-23 01:16:18 +0800

    添加项目文件。

---
+++ RimworldModManager/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace RimworldModManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length==0 || args[0][0]!='-')
+            {
+                Console.WriteLine("Error: No operation specified. (use -h for help)");
+                return;
+            }
+
+            if (args[0].Length == 1)
+            {
+                Console.WriteLine("Error: Argument '-' specified without input. (use -h for help)");
+                return;
+            }
+
+            var operation = args[0];
+            Console.WriteLine(operation);
+        }
+    }
+}
