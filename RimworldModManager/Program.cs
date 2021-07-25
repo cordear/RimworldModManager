0cc347f2bf930ae59dd6136d977d4b24e1891173
cordea <cordear@outlook.com>
2021-07-26 02:20:33 +0800

    Support create modConfig.xml

--- RimworldModManager/Program.cs
+++ RimworldModManager/Program.cs
@@ -25,10 +25,14 @@ namespace RimworldModManager
 
             var operation = args[0][1..]; 
             //Console.WriteLine(operation);
-            switch (operation)
+            switch (operation.First())
             {
-                case "Q":
-                    ListMods();
+                case 'Q':
+                    ListMods(operation);
+                    break;
+                case 'T':
+                    Console.WriteLine("This is a test commend");
+                    CreateModConfigXml();
                     break;
                 default:
                     Console.WriteLine($"Error: Unknown operation '{operation}'.");
@@ -36,7 +40,86 @@ namespace RimworldModManager
             }
         }
 
-        static void ListMods()
+        static void ListMods(string operation)
+        {
+            var currentDir = Environment.CurrentDirectory;
+            if (operation.Length == 1)
+            {
+                if (!File.Exists(currentDir + "\\modConfig.xml"))
+                {
+                    Console.WriteLine("There is no modConfig.xml in current directory. Now creating...");
+                    CreateModConfigXml();
+                }
+            }
+            else
+            {
+                switch (operation[1])
+                {
+                    case 'y':
+                        if (File.Exists(currentDir + "\\modConfig.xml"))
+                        {
+                            Console.Write("modConfig.xml already exist. Recreate? [Y/n]:");
+                            var key= Console.ReadKey();
+                            Console.WriteLine();
+                            while (key.Key != ConsoleKey.Y && key.Key != ConsoleKey.N && key.Key != ConsoleKey.Enter)
+                            {
+                                Console.Write("Illegal input, try again [Y/n]:");
+                                key = Console.ReadKey();
+                                Console.WriteLine();
+                            }
+                            if (key.Key is ConsoleKey.Y or ConsoleKey.Enter)
+                            {
+                                CreateModConfigXml();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Nothing changed.");
+                            }
+                        }
+                        else
+                        {
+                            CreateModConfigXml();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unknown operation. Try again.");
+                        break;
+                }
+                return;
+            }
+
+            List<ModInfo> modInfoList = new List<ModInfo>();
+            var modConfigXml = new XmlDocument();
+            modConfigXml.Load(Environment.CurrentDirectory+"\\modConfig.xml");
+            var rootNode = modConfigXml.SelectSingleNode("Mods");
+            foreach (XmlElement childNode in rootNode.ChildNodes)
+            {
+                modInfoList.Add(new ModInfo(childNode.SelectSingleNode("PackageId").InnerText,
+                    childNode.SelectSingleNode("Id").InnerText,
+                    childNode.SelectSingleNode("Name").InnerText,
+                    DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(childNode.SelectSingleNode("CreateTime").InnerText)).UtcDateTime,
+                    childNode.SelectSingleNode("Status").InnerText=="True"?true:false
+                    ));
+            }
+            //foreach (XmlElement element in knownExpansionsNode)
+            //{
+            //    expansionList.Add(element.InnerText);
+            //}
+
+            ////var maxPackageIdLength = modInfoList.Select(x => x.PackageId.Length).Max();
+            ////var maxNameLength = modInfoList.Select(x => x.Name.Length).Max();
+            ////var maxIdLength = modInfoList.Select(x => x.Id.Length).Max();
+
+            Console.WriteLine($"{"Name",-40}{"Id",-15}{"Create Time",-15}{"Active",-10}");
+            Console.WriteLine($"{"----",-40}{"--",-15}{"-----------",-15}{"------",-10}");
+            foreach (var modInfo in modInfoList)
+            {
+                Console.WriteLine($"{modInfo.Name,-40}{modInfo.Id,-15}" +
+                                  $"{modInfo.CreateTime.ToShortDateString(),-15}{modInfo.IsActive,-10}");
+            }
+        }
+
+        static bool CreateModConfigXml()
         {
             List<string> expansionList = new List<string>();
             List<ModInfo> modInfoList = new List<ModInfo>();
@@ -45,7 +128,7 @@ namespace RimworldModManager
                 .ToString();
             configPath += "\\LocalLow\\Ludeon Studios\\RimWorld by Ludeon Studios\\Config\\ModsConfig.xml";
             var gameModDirPath = "C:\\GOG Games\\RimWorld\\Mods";
-            var modDirs=Directory.GetDirectories(gameModDirPath);
+            var modDirs = Directory.GetDirectories(gameModDirPath);
 
             XmlDocument modsConfigXmlDocument = new XmlDocument();
             modsConfigXmlDocument.Load(configPath);
@@ -59,7 +142,7 @@ namespace RimworldModManager
             {
                 var aboutPath = modDir + "\\About";
                 XmlDocument modAboutXmlDocument = new XmlDocument();
-                modAboutXmlDocument.Load(aboutPath+"\\About.xml");
+                modAboutXmlDocument.Load(aboutPath + "\\About.xml");
                 var modMetaDataNode = modAboutXmlDocument.SelectSingleNode("ModMetaData");
                 var name = modMetaDataNode.SelectSingleNode("name").InnerText;
                 var packageId = modMetaDataNode.SelectSingleNode("packageId").InnerText;
@@ -67,18 +150,14 @@ namespace RimworldModManager
                 string id = idFile.ReadLine();
                 idFile.Close();
                 var createTime = Directory.GetCreationTime(aboutPath + "\\About.xml");
-                modInfoList.Add(new ModInfo(packageId,id,name,createTime));
+                modInfoList.Add(new ModInfo(packageId, id, name, createTime));
             }
 
-            modInfoList=modInfoList.OrderBy(x => x.Name).ToList();
+            modInfoList = modInfoList.OrderBy(x => x.Name).ToList();
 
             foreach (XmlElement childNode in activeModsNode.ChildNodes)
             {
                 modHashSet.Add(childNode.InnerText);
-            }
-            foreach (XmlElement element in knownExpansionsNode)
-            {
-                expansionList.Add(element.InnerText);
             }
 
             foreach (var t in modInfoList)
@@ -88,18 +167,34 @@ namespace RimworldModManager
                     t.IsActive = true;
                 }
             }
+            var modConfigXmlPath = Environment.CurrentDirectory + "\\modConfig.xml";
 
-            //var maxPackageIdLength = modInfoList.Select(x => x.PackageId.Length).Max();
-            //var maxNameLength = modInfoList.Select(x => x.Name.Length).Max();
-            //var maxIdLength = modInfoList.Select(x => x.Id.Length).Max();
-
-            Console.WriteLine($"{"Name",-40}{"Package Id",-45}{"Id",-15}{"Create Time",-10}{"Active",-10}");
-            Console.WriteLine($"{"----",-40}{"----------",-45}{"--",-15}{"-----------",-10}{"------",-10}");
+            var modConfigXml = new XmlDocument();
+            var rootNode = modConfigXml.CreateElement("Mods");
+            modConfigXml.AppendChild(rootNode);
             foreach (var modInfo in modInfoList)
             {
-                Console.WriteLine($"{modInfo.Name,-40}{modInfo.PackageId,-45}{modInfo.Id,-15}" +
-                                  $"{modInfo.CreateTime.ToShortDateString(),-10}{modInfo.IsActive,-10}");
+                var modInfoNode = modConfigXml.CreateElement("ModInfo");
+                var modNameNode = modConfigXml.CreateElement("Name");
+                modNameNode.InnerText = modInfo.Name;
+                var modPackageIdNode = modConfigXml.CreateElement("PackageId");
+                modPackageIdNode.InnerText = modInfo.PackageId;
+                var modIdNode = modConfigXml.CreateElement("Id");
+                modIdNode.InnerText = modInfo.Id;
+                var modStatusNode = modConfigXml.CreateElement("Status");
+                modStatusNode.InnerText = modInfo.IsActive.ToString();
+                var modCreateTimeNode = modConfigXml.CreateElement("CreateTime");
+                modCreateTimeNode.InnerText = Convert.ToString(((DateTimeOffset) modInfo.CreateTime).ToUnixTimeSeconds());
+                modInfoNode.AppendChild(modNameNode);
+                modInfoNode.AppendChild(modPackageIdNode);
+                modInfoNode.AppendChild(modIdNode);
+                modInfoNode.AppendChild(modStatusNode);
+                modInfoNode.AppendChild(modCreateTimeNode);
+                rootNode.AppendChild(modInfoNode);
             }
+            modConfigXml.Save(modConfigXmlPath);
+            Console.WriteLine($"modConfig.xml created. Path:{modConfigXmlPath}");
+            return true;
         }
     }
 }
